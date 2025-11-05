ef6aac5fc83eab5435d17177bf6bde9f44406a02
Xiao <2501683578@qq.com>
2025-11-05 17:22:48 +0800

    fix: http protocol is insecure, use https instead

--- vite.config.ts
+++ vite.config.ts
@@ -16,7 +16,7 @@ export default defineConfig({
     proxy: {
       // 代理所有 /api 请求到后端服务器
       '/api': {
-        target: 'http://47.129.72.211',
+        target: 'https://ceramicraft-customer-frontend',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       }
